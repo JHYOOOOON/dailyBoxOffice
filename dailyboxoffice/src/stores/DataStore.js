30594b3cba28a279bd185462b917c26ba3d01d5b
jhyooooon <jhyooooooon@gmail.com>
2020-03-31 00:23:01 +0900

    Fix scss and date bug

--- dailyboxoffice/src/stores/DataStore.js
+++ dailyboxoffice/src/stores/DataStore.js
@@ -17,9 +17,9 @@ export default class DataStore {
 
   @action getDate() {
     let date = new Date();
-    let year = date.getUTCFullYear();
-    let month = String(date.getUTCMonth() + 1);
-    let day = String(date.getUTCDate());
+    let year = date.getFullYear();
+    let month = String(date.getMonth() + 1);
+    let day = String(date.getDate() - 1);
 
     month = month.length === 1 ? "0" + month : month;
     day = day.length === 1 ? "0" + day : day;
